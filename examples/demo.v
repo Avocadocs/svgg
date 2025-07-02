b5e218b531cc0efb9ca96a46e3548b8b601ff4e4
Kerian SUISINI <97396830+Avocadocs@users.noreply.github.com>
2025-07-02 23:17:32 +0200

    Format files with v fmt

--- examples/demo.v
+++ examples/demo.v
@@ -5,30 +5,29 @@ import svgg
 @[heap]
 struct App {
 mut:
-  img gg.Image
+	img gg.Image
 }
 
 fn main() {
-  mut app := App{}
+	mut app := App{}
 
-  mut ctx := gg.new_context(
-    width: 800
-    height: 600
-    window_title: 'SVGG Example'
-    bg_color: gx.light_blue
-    init_fn: app.init
-    frame_fn: app.frame
-  )
-  ctx.run()
+	mut ctx := gg.new_context(
+		width:        800
+		height:       600
+		window_title: 'SVGG Example'
+		bg_color:     gx.light_blue
+		init_fn:      app.init
+		frame_fn:     app.frame
+	)
+	ctx.run()
 }
 
 fn (mut a App) init(mut ctx gg.Context) {
-  a.img = svgg.create_svg_image(mut ctx, './23.svg', svgg.RasterizeCfg{})
+	a.img = svgg.create_svg_image(mut ctx, './23.svg', svgg.RasterizeCfg{})
 }
 
 fn (mut a App) frame(mut ctx gg.Context) {
-  ctx.begin()
-  ctx.draw_image(0, 0, a.img.width, a.img.height, a.img)
-  ctx.end()
+	ctx.begin()
+	ctx.draw_image(0, 0, a.img.width, a.img.height, a.img)
+	ctx.end()
 }
-
--- nanosvg.c.v
+++ nanosvg.c.v
@@ -9,9 +9,9 @@ module svgg
 
 @[typedef]
 struct C.NSVGimage {
-  width f32
-  height f32
-  shapes voidptr
+	width  f32
+	height f32
+	shapes voidptr
 }
 
 struct C.NSVGrasterizer {}
@@ -20,5 +20,5 @@ fn C.nsvgParseFromFile(path &char, units &char, dpi f32) &C.NSVGimage
 fn C.nsvgDelete(image &C.NSVGimage)
 fn C.nsvgCreateRasterizer() &C.NSVGrasterizer
 fn C.nsvgRasterize(rast &C.NSVGrasterizer, image &C.NSVGimage, ox f32, oy f32,
-                   scale f32, dst &u8, w int, h int, stride int)
+	scale f32, dst &u8, w int, h int, stride int)
 fn C.nsvgDeleteRasterizer(rast &C.NSVGrasterizer)
